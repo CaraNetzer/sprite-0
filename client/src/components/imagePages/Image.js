a9e91baa3d447071f784d4a4aac15089d3b3f21b
CaraNetzer <cara.netzer@gmail.com>
2022-12-12 12:07:23 -0500

    hover icons progress

--- client/src/components/imagePages/Image.js
+++ client/src/components/imagePages/Image.js
@@ -1,5 +1,5 @@
 import React from "react";
-import { Card, CardImg, CardBody } from "reactstrap";
+import { Container, Row, Col } from "reactstrap";
 import { useNavigate } from "react-router-dom";
 import { Image } from "cloudinary-react";
 
@@ -10,8 +10,34 @@ export const SingleImage = ({ image }) => {
 	const navigate = useNavigate();
 
 	return (
-		<figure className="board-image tile is-child ">		
+		<figure className="board-image tile is-child">
+
+
 			<img onClick={() => navigate(`/image/${image.id}`)} className="image" src={image.src}></img>
+
+			<Container className="not-hover">
+				<Row className="row">
+					<Col className="on-top">
+						<i onClick={() => {
+							let elem = document.querySelector("heart fa fa-heart-o");
+							elem.classList.toggle("fa-heart fa-heart-o");
+						}} className="icon heart fa fa-heart-o"></i>
+					</Col>
+					<Col className="on-top">
+						<i onClick={() => {
+							let elem = document.querySelector("fa fa-long-arrow-up");
+							elem.classList.toggle("outlined-arrow");
+						}} className=" icon fa fa-long-arrow-up"></i>
+						<span className="hover-clickable"> {image.upvotes} </span>
+						<i onClick={() => {
+							let elem = document.querySelector("fa fa-long-arrow-down");
+							elem.classList.toggle("outlined-arrow");
+						}} className=" icon fa fa-long-arrow-down"></i>
+						<span className="hover-clickable"> {image.downvotes} </span>
+					</Col>
+				</Row>
+			</Container>
+
 		</figure>
 	);
 };
